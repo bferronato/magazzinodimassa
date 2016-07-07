b0a0771407b15ece5da0d725d61874659fa97180
Bruno <bferronato@gmail.com>
2016-07-07 00:15:50 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -62,7 +62,8 @@
                 	<h1 class="art-logo-name"><a href="./index.php">Calculadora de pre&ccedil;os de produtos</a></h1>
                 	<h6>
                 		<?php 
-                		$my_env_var = getenv('HOST');
+                		$host = getenv('HOST');
+                		echo $host;
                 		?>
                 	</h6>
                     <h2 class="art-logo-text">por c&oacute;digo de barras</h2>
